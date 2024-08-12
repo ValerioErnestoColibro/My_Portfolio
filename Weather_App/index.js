28c43b9af844c2a495c2a4142bd646e4fd3b4e1c
ValerioErnestoColibro <valerio.colibro@gmail.com>
2024-08-12 21:15:43 +0200

    Added modified file

--- Weather_App/index.js
+++ Weather_App/index.js
@@ -5,7 +5,7 @@ const weatherDetails = document.querySelector(".weather-details");
 const error404 = document.querySelector(".not-found");
 
 search.addEventListener("click", () => {
-  const APIKey = "78680af56ca5e86ac3d068bcf77575cb";
+  const APIKey = "ExampleAPI";
   const city = document.querySelector(".search-box input").value;
 
   if (city === "") return;
